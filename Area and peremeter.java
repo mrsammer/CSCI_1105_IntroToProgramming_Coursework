73aa7b6b919594f89d7d11c1153941e4c293b614
mrsammer <46634577+mrsammer@users.noreply.github.com>
2019-05-23 14:53:12 -0600

    Add files via upload

---
+++ Area and peremeter.java
@@ -0,0 +1,11 @@
+/*Author: Samuel Jones
+Date:5/23/2019
+
+This program shows the perimeter of a rectangle
+*/
+class Area and Perimeter {
+	public static void main(String[] args) {
+		System.out.println("The area of a rectangle with the width 4.8 and height 7.9 is " + (4.5 * 7.9));
+		System.out.println("The perimeter of a rectangle with a width of 4.5 and a height of 7.9 comes out to " + (7.9 + 7.9 + 4.5 + 4.5));
+	}
+}
---
+++ Java-1 (1).java
@@ -0,0 +1,10 @@
+/*
+Author: Samuel Jones
+Date: 05/22/2019
+This program shows the output for the fraction
+*/
+class Java {
+	public static void main(String[] args) {
+		System.out.println((9.5 * 4.5 -2.5 * 3) / (45.5 - 3.5));
+	}
+}
